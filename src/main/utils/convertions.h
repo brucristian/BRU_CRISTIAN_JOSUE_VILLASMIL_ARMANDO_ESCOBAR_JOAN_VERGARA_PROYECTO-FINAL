a39bd70bef41e52f1050f0689599fa4db1cef136
Joan Sebastian <joansebastianvergaravalencia@gmail.com>
2025-11-08 17:19:01 -0500

    Agregue la documentacion de validaciones

--- src/main/utils/convertions.h
+++ src/main/utils/convertions.h
@@ -6,6 +6,16 @@
 
 using namespace std;
 
+/**
+ * @brief Convierte una cadena numérica a un entero.
+ * 
+ * Utiliza un flujo de cadena (stringstream) para transformar 
+ * un valor de tipo string en un número entero.
+ * 
+ * @param number Cadena que contiene el número a convertir.
+ * @return int Valor entero equivalente al contenido de la cadena.
+ */
+
 int integerConvertion(const string &number) {
 	int n;
 	
