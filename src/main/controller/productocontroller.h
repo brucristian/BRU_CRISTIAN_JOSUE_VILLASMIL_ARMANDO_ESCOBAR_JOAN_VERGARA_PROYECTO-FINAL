3e59b5b1ad215d87d8858c14ce6fc8c973eb6eed
armandoe02 <armandoescobargentile@gmail.com>
2025-11-11 01:36:34 -0500

    Emñece con mi parte del codigo

---
+++ src/main/controller/productocontroller.h
@@ -0,0 +1,16 @@
+#ifndef PRODUCTOCONTROLLER_H
+#define PRODUCTOCONTROLLER_H
+
+#include "../model/producto.h"
+#include "../utils/filemanager.h"
+
+bool agregarProducto(long long &id, char name[50], double &precio, int &monto){
+
+
+    
+}
+
+
+
+
+#endif
---
+++ src/main/model/producto.h
@@ -0,0 +1,16 @@
+#ifndef PRODUCTO_H
+#define PRODUCTO_H
+
+struct Producto{
+
+    long long id;
+	char name[50];
+	double precio;
+	int monto;
+};
+
+
+
+
+
+#endif
